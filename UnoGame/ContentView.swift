140f5fa27ba4f75c6a92b4cc38e64ae2579d0e5d
jdelano <jdelano@cedarville.edu>
2026-08-19 13:05:55 -0400

    W01D1

--- UnoGame/ContentView.swift
+++ UnoGame/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.white)
+                .strokeBorder(.black)
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.black)
+                .padding(10)
+            Ellipse()
+                .fill(Color.red)
+                .rotationEffect(Angle(degrees: 45))
+                .frame(width: 150, height: 200)
+            Text("UNO")
+                .font(.system(size: 60))
+                .bold()
+                .foregroundStyle(
+                    LinearGradient(
+                        colors: [.yellow, .white],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+                .rotationEffect(Angle(degrees: -10))
+                .shadow(color: .black, radius: 5, x: -3, y: 5)
         }
-        .padding()
+        .frame(width: 200, height: 300)
     }
 }
 
