206f47bd83d02bb483980a5e275dbbf8dcfa3329
jdelano <jdelano@cedarville.edu>
2026-08-24 12:55:11 -0400

    W02D1

--- UnoGame/ContentView.swift
+++ UnoGame/ContentView.swift
@@ -9,31 +9,79 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color.white)
-                .strokeBorder(.black)
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color.black)
-                .padding(10)
-            Ellipse()
-                .fill(Color.red)
-                .rotationEffect(Angle(degrees: 45))
-                .frame(width: 150, height: 200)
-            Text("UNO")
-                .font(.system(size: 60))
-                .bold()
-                .foregroundStyle(
-                    LinearGradient(
-                        colors: [.yellow, .white],
-                        startPoint: .top,
-                        endPoint: .bottom
+        HStack {
+            // UNO Back of Card
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.white)
+                    .strokeBorder(.black)
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.black)
+                    .padding(10)
+                Ellipse()
+                    .fill(Color.red)
+                    .rotationEffect(Angle(degrees: 45))
+                    .frame(width: 150, height: 200)
+                Text("UNO")
+                    .font(.system(size: 60))
+                    .bold()
+                    .fixedSize()
+                    .foregroundStyle(
+                        LinearGradient(
+                            colors: [.white, .yellow],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
                     )
-                )
-                .rotationEffect(Angle(degrees: -10))
-                .shadow(color: .black, radius: 5, x: -3, y: 5)
+                    .rotationEffect(Angle(degrees: -10))
+                    .shadow(color: .black, radius: 5, x: -3, y: 5)
+            }
+            .frame(width: 200, height: 300)
+            
+            // UNO Front of Card
+            ZStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(.white)
+                    .strokeBorder(.black)
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(.blue)
+                    .padding(10)
+                Ellipse()
+                    .fill(.white)
+                    .rotationEffect(Angle(degrees: 45))
+                    .frame(width: 150, height: 200)
+                Text("7")
+                    .font(.system(size: 100))
+                    .bold()
+                    .foregroundStyle(.blue)
+                VStack {
+                    HStack {
+                        Text("7")
+                            .font(.system(size: 30))
+                            .bold()
+                            .foregroundStyle(.white)
+                        
+                        Spacer()
+                    }
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Text("7")
+                            .font(.system(size: 30))
+                            .bold()
+                            .foregroundStyle(.white)
+                            .rotationEffect(Angle(degrees: 180))
+
+                    }
+                }
+                .padding(20)
+                    
+                
+                    
+            }
+            .frame(width: 200, height: 300)
+
         }
-        .frame(width: 200, height: 300)
     }
 }
 
